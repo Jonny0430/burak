929cba3a14de8fcbe884fb972feac0690c5ee8f9
Jonny0430 <ahadjon697gmail.com>
2024-09-03 19:58:20 +0900

    feat: express integration

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,19 @@
-// console.log("EXUCUTED");
-
-// import moment from 'moment';
-// const currentTime = moment().format("YYYY MM DD");
-// console.log(currentTime);
-
-// const person: string = "Jonny";
-// const count: number = 100;
-
-    //Architectural pattern: MVC, Dependency Injection,MVP
+//Architectural pattern: MVC, Dependency Injection,MVP
 
     //Design pattern: Middleware, Decotar
 
     import dotenv from 'dotenv';
     dotenv.config();
-
-    // console.log("PORT:", process.env.PORT);
-    // console.log("MONGO_URL:", process.env.MONGO_URL);
-
     import mongoose from "mongoose";
+    import app from "./app";
 
     mongoose
     .connect(process.env.MONGO_URL as string, {})
     .then((data) => {
         console.log("MongoDB connection succeed");
         const PORT = process.env.PORT ?? 3003;
+        app.listen(PORT, function () {
+         console.log(`The server is running successfully on port: ${PORT}`);
+        });
     })
     .catch((err) => console.log("ERROR on connection MongoDB", err));
