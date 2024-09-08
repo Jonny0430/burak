90a141ffb0db5c2f0b5c495d28b12ddbcb9108f0
Jonny0430 <ahadjon697gmail.com>
2024-09-08 15:44:57 +0900

    fix:TASK;H2

--- src/train.ts
+++ src/train.ts
@@ -34,3 +34,15 @@
    css => SNAK
    -Error handling
 */ 
+
+// H2-TASK: 
+
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+
+function getDigits(input: string): string {
+    return input.replace(/\D/g, '');
+}
+
+console.log(getDigits("m14i1t")); 
+
