b83334bfb12922b1af3092263c48ca036ab6e6a8
Jonny0430 <ahadjon697gmail.com>
2024-09-28 17:32:19 +0900

    feat:  EJS - Frontend signup jarayonini develop qilamiz

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -41,6 +41,7 @@ restaurantController.getSignup = (req: Request, res:Response) => {
 restaurantController.processSignup = async (req: AdminRequest, res:Response) => {
     try {
         console.log("processSignup");
+        console.log("req.body:", req.body);
         const file = req.file;
         if (!file) 
             throw new Errors(HttpCode.BAD_REQUEST,Message.SOMETHING_WENT_WRONG);
