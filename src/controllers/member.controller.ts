b632fd2aaaf70e9aca24e8133c630e9ba0691455
Jonny0430 <ahadjon697gmail.com>
2024-10-17 14:56:17 +0900

    feat: develop token creation via jwt

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -3,18 +3,22 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors from "../libs/Errors";
+import AuthService from "../models/Auth.service";
+
 
 const memberService = new MemberService();
-
+const authService = new AuthService();
 const memberController: T = {};
 
 
 memberController.signup = async (req: Request, res:Response) => {
     try {
         console.log("signup");
-         const input: MemberInput = req.body,
+        const input: MemberInput = req.body,
          result: Member = await memberService.signup(input);
-      
+        const token = await authService.createToken(result);
+        console.log("token:", token);
+         
         res.json({member: result});
     } catch (err) {
         console.log("Error, processSignup:", err);
@@ -28,7 +32,9 @@ memberController.login = async (req: Request, res:Response) => {
     try {
         console.log("login");
         const input: LoginInput = req.body,
-         result = await memberService.login(input);
+         result = await memberService.login(input),
+         token = await authService.createToken(result);
+        console.log("token =>", token); 
 
         res.json({member: result});
     } catch (err) {
